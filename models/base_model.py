6b77398c16909182d9ae14e323a7807405f38b33
sarachafikidrissi <chafikidrissisara@gmail.com>
2023-12-18 18:49:27 +0100

    task 6 updates

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,12 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 import models
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 Base = declarative_base()  # Classe de base SQLAlchemy
 
@@ -16,19 +19,25 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != "__class__":
+                    setattr(self, k, v)
+            if kwargs.get('created_at', None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs['created_at'], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get('updated_at', None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs['updated_at'], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -38,7 +47,8 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
