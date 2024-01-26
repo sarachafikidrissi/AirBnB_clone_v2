62e0d21dfea0d597104d89af2234a2b0776c9279
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-26 17:32:52 +0000

    task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This is 0-hello_route.py model
+hat starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start a basic Flask web application"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
