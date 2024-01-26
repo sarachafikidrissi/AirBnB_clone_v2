14ab551e86d6ffa7a5b4601b12ec7c5bffde78f4
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-26 17:36:40 +0000

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This is 0-hello_route.py model
+hat starts a Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Start a basic Flask web application"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Adding a specific route /hbnb"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
