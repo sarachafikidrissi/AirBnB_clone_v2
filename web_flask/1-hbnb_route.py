8ad8b5ca79d8a28a63d32161af7099d52cda4407
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-01-26 18:32:40 +0000

    updates

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Created on Tue Sep  1 11:15:54 2020
+starts a Flask web application
+"""
 
-@author: Robinson Montes
-"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Start a basic Flask web application"""
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
